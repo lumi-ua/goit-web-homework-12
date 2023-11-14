f972bffd41217fe2b28e729cfdff5a1fae7eb739
lumi-ua <tanichka.chirkova@gmail.com>
2023-11-14 18:14:34 +0200

    initial commit

--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import datetime, date
 from typing import List, Optional
 from pydantic import BaseModel, EmailStr, Field
 
@@ -18,7 +18,34 @@ class ContactResponse(BaseModel):
     phone_number: str
     birthday: date
     
-    # class Config:
-    #     orm_mode = True
+    class Config:
+        from_attributes = True
+
+class UserModel(BaseModel):
+    username: str = Field(min_length=3, max_length=16)
+    email: str
+    password: str = Field(min_length=4, max_length=10)
+
+
+class UserDb(BaseModel):
+    id: int
+    username: str
+    email: str
+    created_at: datetime
+    avatar: str
+
+    class Config:
+        from_attributes = True
+
+
+class UserResponse(BaseModel):
+    user: UserDb
+    detail: str = "User successfully created"
+
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
 
 
