8f87a2a7e5e2aac45cda87d6b86b87492026cf55
lumi-ua <130182058+lumi-ua@users.noreply.github.com>
2023-11-14 18:19:49 +0200

    Update db.py

--- src/database/db.py
+++ src/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:77766@localhost:5432/rest_app2"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:<passwod>@localhost:5432/rest_app2"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
